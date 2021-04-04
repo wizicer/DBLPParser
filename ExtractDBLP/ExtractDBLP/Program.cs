506a637d2fcb5393b117f90f997a7d7fe9dac45b
Icer <liangshuangde@163.com>
2021-04-04 22:06:05 +0800

    clean ccf.json

--- ExtractDBLP/ExtractDBLP/Program.cs
+++ ExtractDBLP/ExtractDBLP/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -8,17 +10,41 @@ namespace ExtractDBLPForm
 {
     static class Program
     {
-       
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            CleanCcf();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmDBLPExtract());
 
+        }
+
+        static void CleanCcf()
+        {
+            var json = File.ReadAllText("ccf.json");
+            var list = JsonConvert.DeserializeObject<CcfRecord[]>(json);
+            foreach (var item in list)
+            {
+                if (item.CrossRef == null || item.CrossRef == "NULL")
+                {
+                    item.CrossRef = null;
+                }
+                else if (!item.CrossRef.EndsWith("/"))
+                {
+                    item.CrossRef += "/";
+                }
+
+                if (item.Title == "NULL") item.Title = null;
+                if (item.Publisher == "NULL") item.Publisher = null;
+            }
+
+            json = JsonConvert.SerializeObject(list, Formatting.Indented, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+            File.WriteAllText(@"..\..\ccf.json", json);
         }
     }
 }
