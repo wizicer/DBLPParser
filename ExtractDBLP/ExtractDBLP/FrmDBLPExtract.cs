26284b729352ca0a552c2d4403a72aacefd528c9
Icer <liangshuangde@163.com>
2021-11-29 15:28:05 +0800

    extract filter by words

--- ExtractDBLP/ExtractDBLP/FrmDBLPExtract.cs
+++ ExtractDBLP/ExtractDBLP/FrmDBLPExtract.cs
@@ -34,7 +34,7 @@
             var wordStats = words.ToDictionary(_ => _, _ => 0);
 
             var rs = GetRecords(this.txtDBLPfile.Text);
-            var fw = Filter(rs)
+            var fw = FilterByWords(rs, words, yearstart, wordStats)
                 .Select(_ => new ExportPaper(_))
                 .ToArray();
             var json = JsonConvert.SerializeObject(
@@ -42,44 +42,43 @@
                 Newtonsoft.Json.Formatting.Indented,
                 new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
             File.WriteAllText(@"..\..\papers.js", "var papers = " + json);
+        }
 
-            IEnumerable<DblpRecord> Filter(IEnumerable<DblpRecord> records)
+        private IEnumerable<DblpRecord> FilterByWords(IEnumerable<DblpRecord> records, string[] words, int yearstart, Dictionary<string, int> wordStats)
+        {
+            var i = 0;
+            var p = 0;
+            foreach (var record in records)
             {
-
-                var i = 0;
-                var p = 0;
-                foreach (var record in records)
+                i++;
+                var isMatch = false;
+                // filter words
+                foreach (var word in words)
                 {
-                    i++;
-                    var isMatch = false;
-                    // filter words
-                    foreach (var word in words)
+                    if (record.title.IndexOf(word, StringComparison.OrdinalIgnoreCase) >= 0)
                     {
-                        if (record.title.IndexOf(word, StringComparison.OrdinalIgnoreCase) >= 0)
+                        isMatch = true;
+
+                        // filter year
+                        if (isMatch && (record is Paper pp && (!int.TryParse(pp.year, out var y) || y < yearstart)))
                         {
-                            isMatch = true;
-
-                            // filter year
-                            if (isMatch && (record is Paper pp && (!int.TryParse(pp.year, out var y) || y < yearstart)))
-                            {
-                                isMatch = false;
-                            }
-
-                            if (isMatch) wordStats[word]++;
+                            isMatch = false;
                         }
-                    }
 
-                    if (isMatch)
-                    {
-                        p++;
-                        yield return record;
+                        if (isMatch) wordStats[word]++;
                     }
-
-                    this.UpdateProgress(i, p);
                 }
 
-                this.UpdateProgress(i, p, true);
+                if (isMatch)
+                {
+                    p++;
+                    yield return record;
+                }
+
+                this.UpdateProgress(i, p);
             }
+
+            this.UpdateProgress(i, p, true);
         }
 
         private void UpdateProgress(int totalProcessed, int found, bool isFinished = false)
