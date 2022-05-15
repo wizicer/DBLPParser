7d30f143b4e051ad6e3152111adfe99b171e4eaa
LiangShuang <liangshuang@sutu.tech>
2022-05-15 15:59:34 +0800

    upgrade to .net 6

--- ExtractDBLP/ExtractDBLP/Program.cs
+++ ExtractDBLP/ExtractDBLP/Program.cs
@@ -18,6 +18,7 @@ namespace ExtractDBLPForm
         static void Main()
         {
             Application.EnableVisualStyles();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmDBLPExtract());
 
